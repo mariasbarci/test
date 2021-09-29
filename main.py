e50d95677e999972dfee20366b2f39f5988ac118
mariasbarci <91467446+mariasbarci@users.noreply.github.com>
2021-09-29 13:15:29 +0300

    Add files via upload
    initial commit

--- main.py
+++ main.py
@@ -1,33 +1,65 @@
+import math
+
 '''
 Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
-  # codul vostru aici
-  
-  
+  flag = "este prim"
+  for x in range(2, int(n / 2) + 1):
+    if n % x == 0:
+      flag = "nu este prim"
+
+  return flag
+
 '''
 Returneaza produsul numerelor din lista lst.
 '''
+
 def get_product(lst):
-  # codul vostru aici
+  print("pb 2")
   
   
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
-  # codul vostru aici
+  print("Prima metoda")
   
   
 '''
 Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
 def get_cmmdc_v2(x, y):
-  # codul vostru aici
+  print("A doua metoda")
   
   
 def main():
-  # interfata de tip consola aici
+  print("Ce problema vreti sa rezolvati?")
+  nrPb = input()
+
+  if int(nrPb) == 1:
+    print("Problema 1. Fie n = ")
+    #citeste date ca sir de caractere
+    str = input()
+
+    #converteste datele de intrare la intreg
+    num = int(str)
+
+    rez = is_prime(num)
+
+    print("Numarul " + str)
+    print(rez)
+
+  elif int(nrPb) == 2:
+    print("Problema 2...")
+    get_product([])
+
+  elif int(nrPb) == 3:
+    x = 0
+    y = 0
+    get_cmmdc_v1(x,y)
+    get_cmmdc_v2(x,y)
+
 
 if __name__ == '__main__':
   main()
